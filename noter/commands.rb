781ed1d7edce3805d700e25eb19cd86c0d6d66db
artemdorozhkin <aa.dorozhkin@ya.ru>
2025-08-02 23:18:55 +0300

    Add modules, fix errors and typos, fix messages

--- noter/commands.rb
+++ noter/commands.rb
@@ -1,12 +1,11 @@
 require_relative 'config'
-include Config
 
 module Commands
-  def del_note(index)
+  def self.del_note(index)
     notes = File.readlines(Config::NOTES_FILE)
     if notes.size == 0
-      puts "You are not write any notes yet"
-      puts "For write new note use:"
+      puts "You haven't written any notes yet"
+      puts "To write a new note, use:"
       puts "  note w <your note text>"
       return
     end
@@ -21,14 +20,14 @@ module Commands
     end
   end
 
-  def clear_notes
+  def self.clear_notes
     if File.exist?(Config::NOTES_FILE)
       File.delete(Config::NOTES_FILE)
     end
-    puts "All notes was cleared"
+    puts "All notes have been cleared"
   end
 
-  def write_note(note)
+  def self.write_note(note)
     notes = File.exist?(Config::NOTES_FILE)? File.readlines(Config::NOTES_FILE) : []
     File.open(Config::NOTES_FILE, "w") do |f|
       f.puts note
@@ -36,7 +35,7 @@ module Commands
     end
   end
 
-  def grep_notes(frase)
+  def self.grep_notes(frase)
     notes = File.readlines(Config::NOTES_FILE)
     if notes.size == 0
       return
@@ -46,10 +45,10 @@ module Commands
     puts filtered.join()
   end
 
-  def print_notes(count = 0)
+  def self.print_notes(count = 0)
     unless File.exist?(Config::NOTES_FILE)
-      puts "You are not write any notes yet"
-      puts "For write new note use:"
+      puts "You haven't written any notes yet"
+      puts "To write a new note, use:"
       puts "  note w <your note text>"
       return
     end
--- noter/main.rb
+++ noter/main.rb
@@ -1,60 +1,60 @@
 require 'colorize'
 
 require_relative 'commands'
-include Commands
 require_relative 'config'
-include Config
 
-def usage
-  puts "USAGE: note [command] [arguments]\n\n"
-  puts "COMMANDS:"
-  puts "  write | w [text]\twrites note text into .notes file"
-  puts "  read  | r [n]\t\tread [n] notes from .notes file. If [n] not specified, readings all notes"
-  puts "  grep  | g [word]\tfiltered notes by word"
-  puts "  del   | d [n]\t\tdeletes [n] note"
-  puts "  clear\t\t\tclear all notes"
-end
-
-def err(msg)
-  puts "\nERR: #{msg}".red
-end
-
-def main
-  command = ARGV[0]
-  
-  case command
-  when "w", "write"
-    write_note ARGV[1...ARGV.size].join(" ")
-    puts "note saved into: #{Config::NOTES_FILE}"
-  when "r", "read"
-    if ARGV.size == 1
-      Commands.print_notes
-    else
-      Commands.print_notes ARGV[1]
-    end
-  when "g", "grep"
-    if ARGV.size == 1
-      Commands.print_notes
-    else
-      Commands.grep_notes ARGV[1...ARGV.size].join(" ")
-    end
-  when "d", "del"
-    if ARGV.size == 1
-      usage
-      err "you need to specify number of notes to delete"
-    else
-      Commands.del_notes ARGV[1]
-    end
-  when "clear"
-    Commands.clear_notes
-  when nil
-    usage
-  else
-    usage
-    err "unknown command #{command}"
+module Noter
+  def self.usage
+    puts "USAGE: note [command] [arguments]\n\n"
+    puts "COMMANDS:"
+    puts "  write | w [text]    Writes note text into the .notes file"
+    puts "  read  | r [n]       Reads [n] notes from the .notes file. If [n] is not specified, reads all notes"
+    puts "  grep  | g [word]    Filters notes by the given word"
+    puts "  del   | d [n]       Deletes note number [n]"
+    puts "  clear               Deletes all notes"
   end
-end 
 
-if __FILE__ == $0
-  main
+  def self.err(msg)
+    puts "\nERR: #{msg}".red
+  end
+
+  def self.main
+    command = ARGV[0]
+
+    case command
+    when "w", "write"
+      write_note ARGV[1...ARGV.size].join(" ")
+      puts "Note saved to: #{Config::NOTES_FILE}"
+    when "r", "read"
+      if ARGV.size == 1
+        Commands.print_notes
+      else
+        Commands.print_notes ARGV[1]
+      end
+    when "g", "grep"
+      if ARGV.size == 1
+        Commands.print_notes
+      else
+        Commands.grep_notes ARGV[1...ARGV.size].join(" ")
+      end
+    when "d", "del"
+      if ARGV.size == 1
+        usage
+        err "You need to specify the number of notes to delete"
+      else
+        Commands.del_notes ARGV[1]
+      end
+    when "clear"
+      Commands.clear_notes
+    when nil
+      usage
+    else
+      usage
+      err "Unknown command #{command}"
+    end
+  end 
+
+  if __FILE__ == $0
+    main
+  end
 end
